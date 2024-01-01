f265ef5c45dbd0261163b04e583e1631a21bd1e2
devkng <dkprojects121@gmail.com>
2023-12-31 18:51:50 -0800

    Version 1.0.2

--- app/src/main/java/com/example/taskreminder/MainActivity.java
+++ app/src/main/java/com/example/taskreminder/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private int hi;
+    private int repeatCount = 0 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
